a6eadb743f6267a5228e7bdb80e5cf147e2fdb5f
Juan Carlos <juancho131401@gmail.com>
2018-10-05 01:26:49 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/FuncionPalabras.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FuncionPalabras.dir/main.c.obj"
+  "FuncionPalabras.pdb"
+  "FuncionPalabras.exe"
+  "FuncionPalabras.exe.manifest"
+  "FuncionPalabras.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FuncionPalabras.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
